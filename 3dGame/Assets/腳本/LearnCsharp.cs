906adeab26d9e129f65283f15cc9a0706d8a04d7
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2020-03-31 21:22:42 +0800

    LearnAPI

--- 3dGame/Assets/腳本/LearnCsharp.cs
+++ 3dGame/Assets/腳本/LearnCsharp.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class LearnCsharp : MonoBehaviour
 {
+	[Header("圖片")]
+	public SpriteRenderer image;
+	[Header("攝影機")]
+	public Transform cam;
 
 
 	private void Start()
 	{
 		print("Hello World!");
+		cam.transform.Rotate(0f, 0f, 90.0f, Space.Self);
 	}
 
 	private void Update()
@@ -14,5 +20,9 @@ public class LearnCsharp : MonoBehaviour
 		print("Dlorw OlleH");
 		print(Random.value);
 		Time.timeScale = 0.1f; //設定時間尺寸
+		Cursor.visible = false;
+		image.flipX = true;
+		print(Mathf.Floor(10.5f));
+		
 	}
 }
