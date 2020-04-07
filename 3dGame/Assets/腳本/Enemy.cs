0a58130288b446faa7c3a638813a12aa9a6b9ddb
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2020-04-07 21:15:21 +0800

    程式練習

---
+++ 3dGame/Assets/腳本/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	//prop唯獨屬性{取得,設定}
+	public float hp { get; set; }
+	public float atk
+	{ get	{return 50;}
+
+	}
+	public float def
+	{
+		get { return 50; }
+
+	}
+
+	private float _damage;
+	public float damage
+	{	set{	_damage = Mathf.Clamp( value - def,1,99999);}
+		get{	return _damage;		}
+
+
+	}
+
+
+	void Start()
+    {
+        
+    }
+
+
+}
---
+++ 3dGame/Assets/腳本/EnemyManger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemyManger : MonoBehaviour
+{
+
+
+	public Enemy enemy;
+
+	void Start()
+    {
+		print(enemy.atk);
+		enemy.damage = 500;
+		print(enemy.damage);
+    }
+
+    
+}
