8d064713f6b85bf665027a67329a5b0cb44b36e9
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2020-04-14 21:34:46 +0800

    鏡頭控制

---
+++ 3dGame/Assets/腳本/Beetle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Beetle : MonoBehaviour
+{
+	[Header("速度")][Range(1,1000)]
+	public float speed;
+	public Joystick joy;
+	[Header("上限")]
+	public float top = 36;
+	[Header("下限")]
+	public float bottom = 12;
+
+	private void Update()
+	{
+		Move();
+	}
+
+
+	private void Move()
+	{
+		float h = Input.GetAxis("Horizontal")*-1;
+		float v = Input.GetAxis("Vertical")*-1;
+		transform.Translate(h * Time.deltaTime * speed, 0, v * Time.deltaTime* speed);
+
+		float joyh = joy.Horizontal*-1;
+		float joyv = joy.Vertical*-1;
+		transform.Translate(joyh * Time.deltaTime * speed, 0, joyv * Time.deltaTime * speed);
+
+		Vector3 pos = transform.position;
+		pos.x = Mathf.Clamp(pos.x, 25, 74);
+		transform.position = pos;
+	}
+}
---
+++ 3dGame/Assets/腳本/CameraControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+
+
+public class CameraControl : MonoBehaviour
+{
+	[Header("速度"), Range(0, 100)]
+	public float speed = 1.5f;
+
+	/*[Header("上限")]
+	public float top = 36;
+	[Header("下限")]
+	public float bottom = 12;*/
+
+	private Transform player;
+
+
+	void Start()
+	{
+		player = GameObject.Find("Flying beetle").transform;
+
+	}
+
+	private void LateUpdate()
+	{
+		Track();
+	}
+
+	private void Track()
+	{
+		Vector3 posP = player.position;
+		Vector3 posC = transform.position;
+		/*
+		posP.x = 0;
+		posP.y = 18;
+		posP.z += 15;
+		
+
+		posC.z = Mathf.Clamp(posC.z, bottom, top);   //Mathf.Clamp(夾住的值,下限,上限)*/
+		posP.y = 46.4f;
+		posP.z -= 2.6f;
+		transform.position = Vector3.Lerp(posC, posP, 0.3f * Time.deltaTime * speed);
+
+	}
+
+}
