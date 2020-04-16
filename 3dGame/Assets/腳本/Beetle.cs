5c976d1498ef02e92246c973fec2f44c3272ff88
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2020-04-16 21:32:52 +0800

    場景限制 子彈射擊

--- 3dGame/Assets/腳本/Beetle.cs
+++ 3dGame/Assets/腳本/Beetle.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections;
+
 
 public class Beetle : MonoBehaviour
 {
@@ -9,10 +11,26 @@ public class Beetle : MonoBehaviour
 	public float top = 36;
 	[Header("下限")]
 	public float bottom = 12;
+	[Header("子彈")]
+	public GameObject bullet;
+	[Header("攻擊冷卻")]
+	public float cd = 1;
+	[Header("子彈速度")]
+	public float bulspeed = 1;
+	[Header("動畫控制器")]
+	private Animator ani;
+
+	private float timer;
+
+	private void Start()
+	{
+		ani = GetComponent<Animator>();
+	}
 
 	private void Update()
 	{
 		Move();
+		Attack();
 	}
 
 
@@ -27,7 +45,30 @@ public class Beetle : MonoBehaviour
 		transform.Translate(joyh * Time.deltaTime * speed, 0, joyv * Time.deltaTime * speed);
 
 		Vector3 pos = transform.position;
-		pos.x = Mathf.Clamp(pos.x, 25, 74);
+		pos.x = Mathf.Clamp(pos.x, 48, 156);
+		pos.z = Mathf.Clamp(pos.z, 4.85f, 26);
 		transform.position = pos;
 	}
+
+	private void Attack()
+	{
+		timer += Time.deltaTime;
+		if (timer > cd)
+		{
+			timer = 0;
+			ani.SetTrigger("攻擊觸發");
+			StartCoroutine("Delaybullet");
+		}
+	}
+
+	private IEnumerator Delaybullet()
+	{
+		yield return new WaitForSeconds(0.5f);
+		Vector3 posbul = transform.position;
+		posbul.z += 3f;
+		posbul.y += -0.1f;
+		GameObject temp =  Instantiate(bullet, posbul, Quaternion.identity);
+		temp.GetComponent<Rigidbody>().AddForce(0, 0, bulspeed);
+	}
+	
 }
---
+++ 3dGame/Assets/腳本/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[Header("地板陣列")]
+	public Transform[] ter;
+	[Header("移動速度")]
+	[Range(1f, 50f)]
+	public float movespeed = 1.5f;
+
+	/// <summary>
+	/// 移動地板
+	/// </summary>
+	public void MoveTerrain()
+	{
+		for (int i = 0; i < ter.Length; i++)
+		{
+			if (ter[i].position.z < -100)
+			{
+				ter[i].position = new Vector3(0, 0, ter[1 - i].position.z + 100);
+			}
+			ter[i].Translate(0, 0, -movespeed * Time.deltaTime);
+			
+		}
+		
+		
+	}
+	private void FixedUpdate()
+	{
+		MoveTerrain();
+
+	}
+}
