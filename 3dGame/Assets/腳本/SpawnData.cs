8ec2a053c3b38fe4f6a9153d24a1cc2a31bd51cb
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2020-04-30 21:36:00 +0800

    怪物生成管理

---
+++ 3dGame/Assets/腳本/SpawnData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="關卡資料",menuName ="關卡")]
+public class SpawnData : ScriptableObject
+{
+	public SpawnTime[] spawn;
+
+}
+//序列化:顯示在unity 屬性面板(class專用)
+[System.Serializable]
+public class SpawnTime
+{
+	public string name = "時間";
+	public float time;
+	public SpawnMonster[] monsters;
+}
+[System.Serializable]
+public class SpawnMonster
+{
+	public GameObject Monster;
+	public float x;
+}
---
+++ 3dGame/Assets/腳本/SpawnManger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManger : MonoBehaviour
+{
+	public SpawnData Sdata;
+	private Vector3 posmon;
+	private void Start()
+	{
+		
+	}
+
+	private IEnumerator StartSpawn()
+	{
+		for (int i = 0; i < Sdata.spawn.Length; i++)
+		{
+			yield return new WaitForSeconds(Sdata.spawn[i].time);
+			print(Sdata.spawn[i].name);
+			for (int y = 0; y < Sdata.spawn[i].monsters.Length; y++)
+			{
+				posmon = new Vector3(Sdata.spawn[i].monsters[i].x, 19.02f, 15);
+				//Instantiate(Sdata.spawn[i].monsters[i].Monster, posmon);
+			}
+		}
+	}
+}
